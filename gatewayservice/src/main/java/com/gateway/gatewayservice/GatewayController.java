95c439a8509c86ffcc834ce45ad334a2a56f617b
Dickson anyaele <dicksonanyaele1234@gmail.com>
2024-08-15 08:52:02 +0000

    fix: fix gateway not finding kubernetes internally

--- gatewayservice/src/main/java/com/gateway/gatewayservice/GatewayController.java
+++ gatewayservice/src/main/java/com/gateway/gatewayservice/GatewayController.java
@@ -2,6 +2,7 @@ package com.gateway.gatewayservice;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GatewayController {
 
-  private final DiscoveryClient discoveryClient;
-
-  public GatewayController(DiscoveryClient discoveryClient) {
-    this.discoveryClient = discoveryClient;
-  }
+  @Autowired
+  private DiscoveryClient discoveryClient;
 
   @GetMapping
   public String test() {
